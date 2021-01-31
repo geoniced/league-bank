2f48e24cba875ed8dc02891515923302e12e19b3
Ivan Kashin <barbos1701@gmail.com>
2021-01-31 16:54:03 +0300

    Переносит шрифты в src, подключает их в scss и убирает вебшрифт подключение из public/index.html

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/style.scss';
 import App from './components/app/app';
+import(/* webpackPreload: true */ './fonts/roboto-regular.woff2'); // Used for preloading a font
 
 ReactDOM.render(
     <React.StrictMode>
