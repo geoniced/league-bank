0b672fd53669e1d178e48654a5c286229be4c114
Ivan Kashin <barbos1701@gmail.com>
2021-02-02 22:12:08 +0300

    Удаляет моковую валюту

--- src/const.js
+++ src/const.js
@@ -12,39 +12,6 @@ export const Currency = {
   CNY: `CNY`,
 };
 
-export const CurrencyRate = {
-  [Currency.RUB]: {
-    [Currency.USD]: 0.013114,
-    [Currency.EUR]: 0.010835,
-    [Currency.GBP]: 0.009589,
-    [Currency.CNY]: 0.084729,
-  },
-  [Currency.USD]: {
-    [Currency.RUB]: 75.955,
-    [Currency.EUR]: 0.823893,
-    [Currency.GBP]: 0.729661,
-    [Currency.CNY]: 6.4276,
-  },
-  [Currency.EUR]: {
-    [Currency.RUB]: 92.19,
-    [Currency.USD]: 1.214,
-    [Currency.GBP]: 0.886,
-    [Currency.CNY]: 7.802,
-  },
-  [Currency.GBP]: {
-    [Currency.RUB]: 104.096,
-    [Currency.USD]: 1.371,
-    [Currency.EUR]: 1.129,
-    [Currency.CNY]: 8.809,
-  },
-  [Currency.CNY]: {
-    [Currency.RUB]: 11.817,
-    [Currency.USD]: 0.156,
-    [Currency.EUR]: 0.128,
-    [Currency.GBP]: 0.114,
-  },
-};
-
 export const HISTORY = [
   {
     date: `2020-11-25`,
